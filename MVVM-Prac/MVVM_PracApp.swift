6c7111d835feb8ba67e73a2ad114a3b78c00a7d9
mattc <tingyugenius@gmail.com>
2023-05-29 13:47:08 +0800

    Initial Commit

---
+++ MVVM-Prac/MVVM_PracApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_PracApp.swift
+//  MVVM-Prac
+//
+//  Created by matt on 2023/5/29.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_PracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
